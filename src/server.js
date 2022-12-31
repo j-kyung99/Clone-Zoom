83722240fccfed2dbfec1a59fc35be25d03c4de1
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2022-12-31 22:52:44 +0900

    #0.3_Frontend_Setup

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ import express from "express";
 
 const app = express(); // 애플리케이션 생성
 
-console.log("hello");
-
-app.listen(3000); // 3000포트로 접속 성공 시 ~
+app.set("view engine", "pug"); // pug로 view engine 설정
+app.set("views", __dirname + "/views"); // express에 template이 어디 있는지 지정
+app.use("/public", express.static(__dirname + "/public")); // public url을 생성해서 유저에게 파일 공유
+app.get("/", (req, res) => res.render("home")); // home.pug를 render 해주는 route handler 생성
+const handleListen = () => console.log("Listening on http://localhost:3000");
+app.listen(3000, handleListen); // 3000포트로 접속 성공 시 ~
