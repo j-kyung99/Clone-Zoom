eac60ede056de4d73fc79d0b386460548cba6f10
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2023-01-04 14:34:53 +0900

    #1.4_WebSocket_Messages

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,18 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
 // 여기에서의 socket은 서버로의 연결을 의미
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+}); // 서버가 연결되었을 때 발생
+
+socket.addEventListener("message", (message) => {
+  console.log("New message: ", message.data);
+}); // 서버로 부터 메세지를 받을 때 발생
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ❌");
+}); // 서버로 부터 연결이 끊겨을 때 발생
+
+setTimeout(() => {
+  socket.send("hello from the browser!");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -15,10 +15,17 @@ const handleListen = () => console.log("Listening on http://localhost:3000");
 const server = http.createServer(app); // server에 접근할 수 있음(http 서버)
 const wss = new WebSocket.Server({ server }); // http 서버 위에 webSocket 서버를 만들 수 있도록 함
 
-function handleConnection(socket) {
-  console.log(socket);
-  // 여기에서의 socket은 연결된 브라우저를 의미
-}
-wss.on("connection", handleConnection);
+// function handleConnection(socket) {
+//   console.log(socket);
+//   // 여기에서의 socket은 연결된 브라우저를 의미
+// }
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  socket.on("message", (message) => {
+    console.log(message.toString("utf8"));
+  });
+  socket.send("hello!!!");
+});
 
 server.listen(3000, handleListen);
